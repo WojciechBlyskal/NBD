d29fe766ede61bc64b782d5a550f99b6ea3d2421
MagdalenaOzarek <247752@edu.p.lodz.pl>
2024-10-15 23:45:49 +0200

    Update Main.java
    Main dziala

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,21 +1,90 @@
 package org.example;
 
 import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
 public class Main {
-        public static void main(String[] args) {
-            EntityManagerFactory emf = Persistence.createEntityManagerFactory("hotelPersistenceUnit");
-            EntityManager em = emf.createEntityManager();
-//            EntityTransaction transaction = em.getTransaction();
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hotelPersistenceUnit");
+        EntityManager em = emf.createEntityManager();
+
+        Guest guest1 = new Guest("Jan", "Kowalski", "123456789");
+        Guest guest2 = new Guest("Marek", "Nowak", "987654321");
+        Guest guest3 = new Guest("Zofia", "Nowak", "456123789");
+
+        Studio studio1 = new Studio(12, 3, 30.13, true, 244.99);
+        MultipleRoom multipleRoom1 = new MultipleRoom(8, 1, 50.78, true, 380.5, 2);
+        MicroSuite microSuite1 = new MicroSuite(56, 6, 18.28, false, 59.9);
+
+        LocalDateTime startTime1 = LocalDateTime.of(2024, 10, 15, 12, 30); // 15 października 2024, 12:30
+        LocalDateTime startTime2 = LocalDateTime.of(2022, 5, 19, 20, 12);
+        Rent rent1 = new Rent("r1", startTime1, microSuite1, guest1);
+        Rent rent2 = new Rent("r2", startTime2, multipleRoom1, guest2);
+
+        GuestRepository guestRepository = new GuestRepository();
+        RoomRepository roomRepository = new RoomRepository();
+        RentRepository rentRepository = new RentRepository();
+
+        guestRepository.addGuest(guest1, em);
+        guestRepository.addGuest(guest2, em);
+        guestRepository.addGuest(guest3, em);
+
+        roomRepository.addRoom(studio1, em);
+        roomRepository.addRoom(multipleRoom1, em);
+        roomRepository.addRoom(microSuite1, em);
+
+        rentRepository.addRent(rent1, em);
+        rentRepository.addRent(rent2, em);
 
 
-            Guest guest = new Guest("Jan", "Kowalski", "123456789");
-            GuestRepository guestRepository = new GuestRepository();
-            guestRepository.addGuest(guest, em);
-            guestRepository.getAllGuests(em);
+        List<Guest> guests = guestRepository.getAllGuests(em);
+
+        for (Guest guest : guests) {
+            System.out.println("Guest ID: " + guest.getId());
+            System.out.println("Name: " + guest.getName());
+            System.out.println("Last Name: " + guest.getLastName());
+            System.out.println("Phone Number: " + guest.getPhoneNumber());
+            System.out.println("---------------------------------");
+        }
+
+
+        List<Guest> guests2 = guestRepository.getGuestByLastName(em, "Kowalski");
+        for (Guest guest : guests2) {
+            System.out.println("Guest ID: " + guest.getId());
+            System.out.println("Name: " + guest.getName());
+            System.out.println("Last Name: " + guest.getLastName());
+            System.out.println("Phone Number: " + guest.getPhoneNumber());
+            System.out.println("---------------------------------");
+        }
+        guest1.setName("Piotr");
+
+        List<Guest> guests3 = guestRepository.getGuestByLastName(em, "Kowalski");
+
+        for (Guest guest : guests3) {
+            System.out.println("Guest ID: " + guest.getId());
+            System.out.println("Name: " + guest.getName());
+            System.out.println("Last Name: " + guest.getLastName());
+            System.out.println("Phone Number: " + guest.getPhoneNumber());
+            System.out.println("---------------------------------");
+        }
+
+//            System.out.println(guestRepository.getGuestByLastName(em, "Kowalski"));
 
 
 
 
+
+//            roomRepository.getAllRooms(em);
+//            rentRepository.getAllRents(em);
+
+
+
+
+
+        em.close();
+        emf.close();
 
 
 
@@ -57,6 +126,6 @@ public class Main {
 
 
 
-        }
+    }
 
 }
